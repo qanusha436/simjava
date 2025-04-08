de60862a02096b46e270ebccb02facdd33b202f1
qanusha436 <qanusha436@gmail.com>
2025-04-08 11:51:10 +0530

    saved

---
+++ add1.java
@@ -0,0 +1,8 @@
+public class add1{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("the addition of number is:"+sum);
+    }
+}
