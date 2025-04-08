ee24d417c52bd082600ec22f07b20491c0467972
qanusha436 <qanusha436@gmail.com>
2025-04-08 11:57:58 +0530

    saved

--- add1.java
+++ add1.java
@@ -2,7 +2,7 @@ public class add1{
     public static void main(String[] args){
         int a=10;
         int b=20;
-        int sum=a+b;
-        System.out.println("the addition of number is:"+sum);
+        int sub=a-b;
+        System.out.println("substration of two number is:"+sub);
     }
 }
